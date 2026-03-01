50affd8483636a69acb2a2771ae13e5ea34d033f
Tiaaan12 <christiandevera232@gmail.com>
2026-03-01 23:37:48 +0800

    [Training] : model training + saving local changes

--- src/preprocess.py
+++ src/preprocess.py
@@ -5,17 +5,27 @@ import seaborn as sns
 
 def load_process(path : str) -> pd.DataFrame:
     data = pd.read_csv(path)
-    print(data.info()) # see the count values and dtype for each feature
-    print(data.isnull().sum()) # check if there are null values
-    print(data['occured'].value_counts(normalize=True)) # check if the target values are balance
+    
+    #print(data.info()) # see the count values and dtype for each feature
+    #print(data.isnull().sum()) # check if there are null values
+    #print(data['occured'].value_counts(normalize=True)) # check if the target values are balance
 
     # remove unecessary features
     data = data.drop(['Total Deaths', 'Total Affected', 'Disaster Type', 'time', 'distance'], axis=1)
     
     # feature engineering - think of releveant correlation
-    data["rainfall_elevation"] = data["Rainfall"] / (data['Elevation'] + 1)
-    data["terrain_risk"] = data["Rainfall"] / (data["Elevation"] - data["Elevation"].min() + 10)
-    data["rain_slope"] = data['Rainfall'] * data["Slope"]
+    elevation_safe = data['Elevation'] + 1
+
+    data['rainfall_elevation'] = data['Rainfall'] / elevation_safe
+    data['rainfall_elevation'] = data['rainfall_elevation'].clip(0, 500)
+
+    elevation_range = data['Elevation'].max() - data['Elevation'].min() + 1
+    data['terrain_risk'] = data['Rainfall'] / elevation_range
+    data['terrain_risk'] = data['terrain_risk'].clip(0, 50)
+
+    slope_normalized = data['Slope'] / (data['Slope'].max() + 1)
+    data['rain_slope'] = data['Rainfall'] * slope_normalized
+    data['rain_slope'] = data['rain_slope'].clip(0, 5000)
     
     for col in ['rainfall_elevation', 'terrain_risk', 'rain_slope']:
         min_val = data[col].min()
--- src/train.py
+++ src/train.py
@@ -1,19 +1,47 @@
 from pathlib import Path
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
 from sklearn.preprocessing import StandardScaler
 from preprocess import load_process
+from imblearn.over_sampling import SMOTE
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 RAW_DATA = BASE_DIR / "data" / "raw" / "flood_dataset_classification.csv"
 MODEL_PATH = BASE_DIR / "models" / "model.pkl"
 
 def train():
-    
+
     data = load_process(RAW_DATA)
     
-    print(data)
+    X = data.drop(['occured'], axis=1)
+    y = data['occured']
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    
+    scaler = StandardScaler()
+    X_train_s = scaler.fit_transform(X_train)
+    X_test_s = scaler.transform(X_test)
+    
+    xgb = XGBClassifier(
+    scale_pos_weight=944/304,
+    n_estimators=500,
+    colsample_bytree=0.6,
+    max_depth=10,
+    learning_rate=0.1,
+)
 
+    sm = SMOTE(random_state=42)
+    X_train_res, y_train_res = sm.fit_resample(X_train_s, y_train)
+    xgb.fit(X_train_res, y_train_res)
+    
+    y_pred = xgb.predict(X_test_s)
+    
+    print(xgb.score(X_test_s, y_test))
+    
+  
+    
+    
 if __name__ == "__main__":
     train() 
     
