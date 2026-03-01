4cb8b2f833ce6d24d73a622ca491267c92fcfe2f
Tiaaan12 <christiandevera232@gmail.com>
2026-03-01 20:01:05 +0800

    [Processing] : remove unrelevant features

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,9 +4,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = pd.read_csv("data/raw/flood_dataset_classification.csv")
-print(data.info()) # See the count values and dtype for each feature
+print(data.info()) # see the count values and dtype for each feature
 print(data.isnull().sum()) # check if there are null values
-print(data['occured'].value_counts)
+print(data['occured'].value_counts(normalize=True)) # check if the target values are balance
+
+# remove unecessary features
+data = data.drop(['Total_Deaths', 'Total Affected', 'Disaster_type', 'time', 'distance'], axis=1)
+
+
 
 
 
