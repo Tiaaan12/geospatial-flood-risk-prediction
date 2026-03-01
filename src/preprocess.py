d463b226d2116dcf9c37099ce29373496f5b7dfd
Tiaaan12 <christiandevera232@gmail.com>
2026-03-01 21:31:03 +0800

    [Processing] : feature engineering relavant correlation

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,16 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data = pd.read_csv("data/raw/flood_dataset_classification.csv")
-print(data.info()) # see the count values and dtype for each feature
-print(data.isnull().sum()) # check if there are null values
-print(data['occured'].value_counts(normalize=True)) # check if the target values are balance
+def load_process(path : str) => pd.DataFrame:
+    data = pd.read_csv("data/raw/flood_dataset_classification.csv")
+    print(data.info()) # see the count values and dtype for each feature
+    print(data.isnull().sum()) # check if there are null values
+    print(data['occured'].value_counts(normalize=True)) # check if the target values are balance
 
-# remove unecessary features
-data = data.drop(['Total Deaths', 'Total Affected', 'Disaster Type', 'time', 'distance'], axis=1)
+    # remove unecessary features
+    data = data.drop(['Total Deaths', 'Total Affected', 'Disaster Type', 'time', 'distance'], axis=1)
 
-data.replace([np.inf, -np.inf], np.nan, inplace=True)
-data.fillna(data.median(numeric_only=True), inplace=True)
+    # handle missing values
+    data.replace([np.inf, -np.inf], np.nan, inplace=True)
+    data.fillna(data.median(numeric_only=True), inplace=True)
 
 
 
