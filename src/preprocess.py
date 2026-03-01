5107ddc9e6c876877cbd6b37dc0147c4ec999947
Tiaaan12 <christiandevera232@gmail.com>
2026-03-01 21:41:50 +0800

    [Processing] : handling negative values and using log to reduce massive outliers

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def load_process(path : str) => pd.DataFrame:
+def load_process(path : str) -> pd.DataFrame:
     data = pd.read_csv("data/raw/flood_dataset_classification.csv")
     print(data.info()) # see the count values and dtype for each feature
     print(data.isnull().sum()) # check if there are null values
@@ -11,10 +11,24 @@ def load_process(path : str) => pd.DataFrame:
 
     # remove unecessary features
     data = data.drop(['Total Deaths', 'Total Affected', 'Disaster Type', 'time', 'distance'], axis=1)
+    
+    # feature engineering - think of releveant correlation
+    data["rainfall_elevation"] = data["Rainfall"] / (data['Elevation'] + 1)
+    data["terrain_risk"] = data["Rainfall"] / (data["Elevation"] - data["Elevation"].min() + 10)
+    data["rain_slope"] = data['Rainfall'] * data["Slope"]
+    
+    for col in ['rainfall_elevation', 'terrain_risk', 'rain_slope']:
+        min_val = data[col].min()
+        shift = 0
+        if min_val <= 0:
+            shift = abs(min_val) + 1
+        data[col] = np.log1p(data[col] + shift)
 
     # handle missing values
     data.replace([np.inf, -np.inf], np.nan, inplace=True)
     data.fillna(data.median(numeric_only=True), inplace=True)
+    
+    return data
 
 
 
