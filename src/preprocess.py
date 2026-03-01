7f62bf68e360fc3faece3df4df3af900693baefc
Tiaaan12 <christiandevera232@gmail.com>
2026-03-01 19:54:11 +0800

    [Processing] : minor changes + data checking

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,4 +4,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = pd.read_csv("data/raw/flood_dataset_classification.csv")
-print(data)
+print(data.info()) # See the count values and dtype for each feature
+print(data.isnull().sum()) # check if there are null values
+print(data['occured'].value_counts)
+
+
+
+
