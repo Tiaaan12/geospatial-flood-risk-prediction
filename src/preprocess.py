23dd953bd535b509dd7798af87c8883a2cdee254
Tiaaan12 <christiandevera232@gmail.com>
2026-03-01 21:59:01 +0800

    [Processing] : add seaborn for visualization

--- src/preprocess.py
+++ src/preprocess.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def load_process(path : str) -> pd.DataFrame:
-    data = pd.read_csv("data/raw/flood_dataset_classification.csv")
+    data = pd.read_csv(path)
     print(data.info()) # see the count values and dtype for each feature
     print(data.isnull().sum()) # check if there are null values
     print(data['occured'].value_counts(normalize=True)) # check if the target values are balance
@@ -27,6 +27,10 @@ def load_process(path : str) -> pd.DataFrame:
     # handle missing values
     data.replace([np.inf, -np.inf], np.nan, inplace=True)
     data.fillna(data.median(numeric_only=True), inplace=True)
+    
+    # visulization for the correlation of each features
+    plt.figure(figsize=(15,8))
+    sns.heatmap(data.corr(), annot=True, cmap="YlGnBu")
     
     return data
 
