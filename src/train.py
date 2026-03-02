e23c095718c3db3dbd84051697737773655abef4
Tiaaan12 <christiandevera232@gmail.com>
2026-03-02 23:01:53 +0800

    [Training] : saved the model and scaler

--- src/train.py
+++ src/train.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import GridSearchCV
 BASE_DIR = Path(__file__).resolve().parent.parent
 RAW_DATA = BASE_DIR / "data" / "raw" / "flood_dataset_classification.csv"
 MODEL_PATH = BASE_DIR / "models" / "model.pkl"
+SCALER_PATH = BASE_DIR / "models" / "scaler.pkl"
 
 def train():
 
@@ -48,6 +49,7 @@ def train():
         "learning_rate" : [0.05, 0.1]
     }
     
+    # hyperparameters tuning to create the best model
     grid_search = GridSearchCV(xgb_model, params, cv=3, return_train_score=True, verbose=2, n_jobs=-1, scoring="f1_macro")
     grid_search.fit(X_train_s, y_train)
     best_estimators = grid_search.best_estimator_
@@ -55,8 +57,9 @@ def train():
     print(grid_search.best_estimator_)
     print(best_estimators.score(X_test_s, y_test))
     
-    # saving the model
-    #joblib.dump(xgb_model, MODEL_PATH)
+    #saving the model
+    joblib.dump(scaler, SCALER_PATH)
+    joblib.dump(xgb_model, MODEL_PATH)
   
 if __name__ == "__main__":
     train() 
