cee2f3fdf0ca7d5256e0ad231d528ede9a460d11
Tiaaan12 <christiandevera232@gmail.com>
2026-03-04 23:23:00 +0800

    [Predicting] : create a function and dataframe

--- src/predict.py
+++ src/predict.py
@@ -12,3 +12,16 @@ SCALER_PATH = BASE_DIR / "models" / "scaler.pkl"
 
 model = joblib.load(MODEL_PATH)
 scaler = joblib.load(SCALER_PATH)
+
+def predict_single(latitude, longitude, duration, rainfall, elevation, slope):
+
+
+    # create the dataframe with base features
+    data = pd.DataFrame([{
+        "Latitude": latitude,
+        "Longitude": longitude,
+        "duration": duration,
+        "Rainfall": rainfall,
+        "Elevation": elevation,
+        "Slope": slope
+    }])
